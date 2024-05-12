46908b585d757bd09f2cf6e57d9236c665900348
madyapadda <146724965+madyapadda@users.noreply.github.com>
2024-05-13 02:07:58 +0700

    Update satu.py

--- satu.py
+++ satu.py
@@ -1,11 +1,3 @@
-# 1. insert i e: Masukkan bilangan bulat pada posisi .
-# 2. print: Mencetak daftar.
-# 3. remove e: Hapus kemunculan pertama bilangan bulat .
-# 4. append e: Sisipkan bilangan bulat di akhir daftar. 
-# 5. sort: Mengurutkan daftar.
-# 6. pop: Munculkan elemen terakhir dari daftar.
-# 7. reverse: Balikkan daftar.
-
 angka = []
 angka.insert(0, 6)
 angka.insert(1, 5)
@@ -19,3 +11,7 @@ print(angka)
 angka.pop(-1)
 angka.reverse()
 print(angka)
+
+#[6,5,10]
+#[1,5,9,10]
+#[9,5,1]
